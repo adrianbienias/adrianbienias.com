0d53fcdb0e932d160a00a3576bd615e71686a61f
Adrian Bienias <adrianbienias@gmail.com>
2023-11-13 14:12:17 +0100

    Automatic deploy

--- _next/static/qKSzeZdGo_2hvIysawMSr/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a619118c50385b8b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
